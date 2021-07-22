9e82d30f32649fb06262b6197ec41a5f2092ae29
Gene <gene891212@gmail.com>
2021-07-22 15:55:47 +0800

    update webcam version

--- python/predict_webcam.py
+++ python/predict_webcam.py
@@ -3,6 +3,7 @@
 # 2. resize network input size to (w', h')
 # 3. pass the image to network and do inference
 # (4. if inference speed is too slow for you, try to make w' x h' smaller, which is defined with DEFAULT_INPUT_SIZE (in object_detection.py or ObjectDetection.cs))
+import cv2
 import sys
 import tensorflow as tf
 import numpy as np
@@ -35,42 +36,47 @@ class TFLiteObjectDetection(ObjectDetection):
 
 
 
-def main(image_filename):
+def main():
     # Load labels
     with open(LABELS_FILENAME, 'r') as f:
         labels = [l.strip() for l in f.readlines()]
 
     od_model = TFLiteObjectDetection(MODEL_FILENAME, labels)
 
-    image = Image.open(image_filename)
-    predictions = od_model.predict_image(image)
+    cap = cv2.VideoCapture(0)
+    while True:
+        ref, frame = cap.read()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        color_coverted = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image=Image.fromarray(color_coverted)
 
-    # Draw rectangle
-    for pred in predictions:
-        if pred['probability'] > .7:
-            pred_bound = pred['boundingBox']
-            rect_startwith = (pred_bound['left'] * image.width, pred_bound['top'] * image.height)
-            pred_shape = [
-                rect_startwith, 
-                (
-                    rect_startwith[0] + pred_bound['width'] * image.width,
-                    rect_startwith[1] + pred_bound['height'] * image.height
-                )
-            ]
-            draw_img = ImageDraw.Draw(image)
-            draw_img.rectangle(pred_shape, outline='red')
+        predictions = od_model.predict_image(image)
 
-            label = [(pred_shape[0][0], pred_shape[0][1] - 15), (pred_shape[1][0], pred_shape[0][1])]
-            draw_img.rectangle(label, fill='red')
-            font = ImageFont.truetype("arial.ttf", 16)
-            draw_img.text((pred_shape[0][0] + 5, pred_shape[0][1] - 15), pred["tagName"], font=font)
+        # Draw rectangle
+        for pred in predictions:
+            if pred['probability'] > .7:
+                pred_bound = pred['boundingBox']
+                rect_startwith = (pred_bound['left'] * image.width, pred_bound['top'] * image.height)
+                pred_shape = [
+                    rect_startwith, 
+                    (
+                        rect_startwith[0] + pred_bound['width'] * image.width,
+                        rect_startwith[1] + pred_bound['height'] * image.height
+                    )
+                ]
+                draw_img = ImageDraw.Draw(image)
+                draw_img.rectangle(pred_shape, outline='red')
 
-    print(predictions)
-    image.show()
+                label = [(pred_shape[0][0], pred_shape[0][1] - 15), (pred_shape[1][0], pred_shape[0][1])]
+                draw_img.rectangle(label, fill='red')
+                font = ImageFont.truetype("arial.ttf", 16)
+                draw_img.text((pred_shape[0][0] + 5, pred_shape[0][1] - 15), pred["tagName"], font=font)
+        
+        numpy_image=np.array(image)
+        opencv_image=cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR)
+        cv2.imshow('frame', opencv_image)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print('USAGE: {} image_filename'.format(sys.argv[0]))
-    else:
-        main(sys.argv[1])
+    main()
